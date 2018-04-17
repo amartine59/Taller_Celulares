63ef537cef062b94f3fc519247277d82197590e7
Andres <barathum196@gmail.com>
2018-04-16 19:23:25 -0500

    Strings agregados

---
+++ app/src/main/java/com/holamundo/taller_celulares/Celular.java
@@ -0,0 +1,8 @@
+package com.holamundo.taller_celulares;
+
+/**
+ * Created by android on 16/04/2018.
+ */
+
+public class Celular {
+}
---
+++ app/src/main/java/com/holamundo/taller_celulares/Registro_Celular.java
@@ -0,0 +1,13 @@
+package com.holamundo.taller_celulares;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registro_Celular extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro__celular);
+    }
+}
