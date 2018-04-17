69052ebaffd21946d781d169beffed661560e9e7
Andres <barathum196@gmail.com>
2018-04-16 20:00:19 -0500

    Interfaces de usuario terminadas

--- app/src/main/java/com/holamundo/taller_celulares/Principal.java
+++ app/src/main/java/com/holamundo/taller_celulares/Principal.java
@@ -1,13 +1,48 @@
 package com.holamundo.taller_celulares;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Principal extends AppCompatActivity {
+    private ListView lista_principal;
+    private String [] op_menuPrincipal;
+    private Intent in_principal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        lista_principal = findViewById(R.id.lvPrincipal);
+        op_menuPrincipal = getResources().getStringArray(R.array.op_menup);
+
+        ArrayAdapter<String> adp_menup = new ArrayAdapter(this,android.R.layout.simple_list_item_1,op_menuPrincipal);
+        lista_principal.setAdapter(adp_menup);
+        lista_principal.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        in_principal = new Intent(Principal.this,Registro_Celular.class);
+                        startActivity(in_principal);
+                        break;
+
+                    case 1:
+                        in_principal = new Intent(Principal.this,Listado_Celular.class);
+                        startActivity(in_principal);
+                        break;
+
+                    case 2:
+                        in_principal = new Intent(Principal.this,Reportes.class);
+                        startActivity(in_principal);
+                        break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/holamundo/taller_celulares/Reportes.java
+++ app/src/main/java/com/holamundo/taller_celulares/Reportes.java
@@ -2,12 +2,35 @@ package com.holamundo.taller_celulares;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Reportes extends AppCompatActivity {
+    private ListView lista_reportes;
+    private String [] op_menureportes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportes);
+
+        lista_reportes = findViewById(R.id.lvReportes);
+        op_menureportes = getResources().getStringArray(R.array.op_reportes);
+        ArrayAdapter<String> adp_reportes = new ArrayAdapter(this,android.R.layout.simple_list_item_1,op_menureportes);
+        lista_reportes.setAdapter(adp_reportes);
+        lista_reportes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                }
+            }
+        });
+
     }
 }
